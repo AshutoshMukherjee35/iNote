fe8cdb1fc6fe527c6680fb89728e469e988bc312
ashutosh.mukherjee <ashutosh.mukherjee@tadigital.com>
2022-05-25 16:23:22 +0530

    validation check in the backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req,res)=>{
- const obj = {
-       a: 'this',
-       number: 34
+
+//create a user using: POST "/api/auth/". Doesn't require auth
+router.post('/', [
+    body('name', 'Enter valid name').isLength({ min: 3 }),
+    body('password' , 'Password must be atleast 5 characters').isLength({ min: 5 }),
+    body('email', 'Enter Valid Email ID').isEmail()
+] , (req,res)=>{
+    // console.log(req.body);
+    // const user = User(req.body);
+    // user.save()
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj)
+    
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      }).then(user => res.json(user)).catch(error => {console.log(error)
+        res.json({error: 'Please enter a unique value for email', message: error.message})})
+    // res.send(req.body);
 })
 module.exports = router
